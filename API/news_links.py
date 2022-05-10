e3112667f7670bb8cb893aaba9f3d5ad2b972cbb
negative111 <90572530+negative111@users.noreply.github.com>
2022-05-10 19:12:57 +0300

    Add files via upload

--- API/news_links.py
+++ API/news_links.py
@@ -4,6 +4,7 @@ import re
 #626 строчка
 links=[]
 gg=[]
+d=0
 def get_html(url):
     result = requests.get(url)
     return result.text
@@ -16,19 +17,34 @@ d2='col-md-3'
 def get_data(html):
     soup=BeautifulSoup(html, "lxml")
     #в строчке ниже меняешь d1 и d2///////////////////////////////
-    g= soup.find_all("div", class_= d2)
+    g= soup.find_all("div", class_= d1)
     soup = BeautifulSoup(str(g), "lxml")
     g=soup.find_all('a')
     url_pattern = r'/ru[\S]+'
     urls = list(set(re.findall(url_pattern, str(g))))
     for i in range(len(urls)):
-        print('https://physics.itmo.ru'+urls[i])
-
+        c='https://physics.itmo.ru'+str(urls[i][:-2])
+        links.append(c)
+        #print('https://physics.itmo.ru'+urls[i])
+        print(links[i])
 
 def main():
     get_data(html)
 
 if __name__ == "__main__":
     main()
+FIND = "<nppcc"
+with open("NewPhystech_OLD.html", 'r+') as f1, open("NewPhystech.html", 'w+') as f2:
+    x=f1.readlines()
+    for line in x:
+            if FIND in line:
+                f2.write('			    <a href="'+str(links[d])+'"></a></li>'+"\n")
+                d+=1
+            else:
 
+                f2.write(line)
+
+    x=f2.readlines()
+
+print(x)
 
