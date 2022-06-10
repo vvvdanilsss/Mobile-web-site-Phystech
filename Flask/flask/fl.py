ad37bf59ae86dcf63a74491424c70ac2cd2c8a7a
negative111 <90572530+negative111@users.noreply.github.com>
2022-06-10 03:47:34 +0300

    Add files via upload

---
+++ Flask/flask/fl.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__, static_folder="static_dir")
+
+@app.route("/")
+def main():
+    return render_template('NewPhystech.html', title="Новый физтех. Университет ИТМО")
+
+
+@app.route("/seminars")
+def seminars():
+    return render_template('seminars.html')
+
+@app.route("/video")
+def video():
+    return render_template('video.html')
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
+
+@app.route("/staff")
+def staff():
+    return render_template('staff.html')
+
+@app.route("/timetable")
+def timetable():
+    return render_template('timetable.html')
+
+@app.route("/news1")
+def news1():
+    return render_template('z_news_1.html')
+
+@app.route("/news2")
+def news2():
+    return render_template('z_news_2.html')
+
+@app.route("/news3")
+def news3():
+    return render_template('z_news_3.html')
+
+@app.route("/news4")
+def news4():
+    return render_template('z_news_4.html')
+
+@app.route("/news5")
+def news5():
+    return render_template('z_news_5.html')
+
+@app.route("/smiaboutus")
+def smiabobus():
+    return render_template('smiabobus.html')
+
+@app.route("/bacalavriat")
+def bacalavriat():
+    return render_template('bacalavriat.html')
+
+@app.route("/aspirantura")
+def aspirantura():
+    return render_template('aspirantura.html')
+
+@app.route("/vacancy")
+def vacancy():
+    return render_template('vacancy.html')
+
+@app.route("/science-groups")
+def sciencegroups():
+    return render_template('science-groups.html')
+
+@app.route("/publications")
+def publications():
+    return render_template('publications.html')
+
+@app.route("/magistratura")
+def magistratura():
+    return render_template('magistratura.html')
+
+@app.route("/magistreFot")
+def magistreFot():
+    return render_template('magistreFot.html')
+
+@app.route("/magistreRad")
+def magistreRad():
+    return render_template('magistreRad.html')
+
+@app.route("/magistreSovr")
+def magistreSovr():
+    return render_template('magistreSovr.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4567)
---
+++ Flask/flask/static_dir/script.js
@@ -0,0 +1,81 @@
+function open(idElement) {
+    var x = document.getElementById(idElement);
+    x.className += ' open';
+}
+
+function close(idName, className) {
+    var x = document.getElementById(idName);
+    x.className = className;
+}
+
+function openMenu() {
+    open('myBurger');
+    open('myUp');
+    open('myBottom');
+    open('mySite');
+}
+
+function closeMenu() {
+    close('myBurger', 'up__burger');
+    close('myUp', 'head__up');
+    close('myBottom', 'head__bottom');
+    close('mySite', 'body__site');
+}
+
+myBurger.addEventListener('click', e => {
+    var x = document.getElementById('myBurger');
+    if (x.className === 'up__burger') {
+        openMenu()
+    } else {
+        closeMenu()
+    }
+    document.addEventListener('click', e => {
+        const target = e.target;
+        if (!target.closest('.head')) {
+            closeMenu()
+        }
+    })
+})
+
+function clickMenu(element, mainElement) {
+    var x = document.getElementById(element);
+    if (x.className === element) {
+        x.className += ' click';
+    } else {
+        x.className = element;
+    }
+    document.addEventListener('click', e => {
+        const target = e.target;
+        if (!target.closest(mainElement)) {
+            x.className = element;
+        }
+    })
+}
+
+abit.addEventListener('click', e => {
+    clickMenu('abitur', '#abit')
+})
+
+quick.addEventListener('click', e => {
+    clickMenu('quickl', '#quick')
+})
+
+issl.addEventListener('click', e => {
+    clickMenu('issled', '#issl')
+})
+
+onas.addEventListener('click', e => {
+    clickMenu('onewph', '#onas')
+})
+
+window.addEventListener('scroll', e => {
+    var scroll = document.querySelector('.scrollTop')
+    scroll.classList.toggle('active', window.scrollY > 100)
+})
+
+function myScrollTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
